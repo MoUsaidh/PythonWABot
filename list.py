4fbf2aa07e1c16562a5d5566a9ed6a38df331163
MoUsaidh <usaidhsuhail@gmail.com>
2022-11-30 10:27:41 +0530

    commit 2

--- list.py
+++ list.py
@@ -1,14 +1,9 @@
 import tkinter
 from tkinter import ttk
-import list
 
-
-class MainMenu:
+class List:
     def __init__(self):
         self.menu()
-
-
-
 
     @staticmethod
     def menu():
@@ -19,20 +14,19 @@ class MainMenu:
 
         # elements
         # title label
-        label1 = tkinter.Label(master=menu, text="WABOT", width=8, padx=10, pady=10, font=('Consolas',14))
-        label1.grid(row=0, column=1)
+
         # buttons
-        button_send = tkinter.Button(master=menu, text="SEND NOW", width=8, padx=10, pady=10)
+        button_send = tkinter.Button(master=menu, text="Add", width=8, padx=10, pady=10)
         button_send.grid(row=1, column=0)
 
-        button_sendlater = tkinter.Button(master=menu, text="SEND LATER", width=8, padx=10, pady=10)
+        button_sendlater = tkinter.Button(master=menu, text="Delete", width=8, padx=10, pady=10)
         button_sendlater.grid(row=1, column=1)
 
-        button_list = tkinter.Button(master=menu, text="LIST", width=5, padx=10, pady=10, command=list.List)
+        button_list = tkinter.Button(master=menu, text="Update", width=5, padx=10, pady=10)
         button_list.grid(row=1, column=2)
 
         # frame
-        frame1 = tkinter.LabelFrame(master=menu, text="Upcoming Events",padx=4, pady=4, font=('Consolas',14))
+        frame1 = tkinter.LabelFrame(master=menu,padx=4, pady=4, font=('Consolas',14))
         frame1.grid(row=3, column=0,columnspan=3)
 
         # table
